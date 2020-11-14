36d9ca5ed2d1d6cbdd7950440d83d74bd374c9a9
RX-0-95 <68135665+RX-0-95@users.noreply.github.com>
2020-11-13 19:19:24 -0800

    move mode to data folder

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-OPENCV_DATA_PATH = "H:\OpencvSourceCode\opencv\data\haarcascades"
+#OPENCV_DATA_PATH = "H:\OpencvSourceCode\opencv\data\haarcascades"
+#HASS_FRONTAL_FACE = "\haarcascade_frontalface_default.xml"
+
+OPENCV_DATA_PATH = "data"
 HASS_FRONTAL_FACE = "\haarcascade_frontalface_default.xml"
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Dependency 
+pip install pyqt5
+pip install opencv-python (pip install opencv-python-headless (for mac)) 
+
+
+## Run the project 
+run the run.py
